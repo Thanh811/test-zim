9c18291ef1a1cb6f83b45622068b750aefa1ba81
LeThanh <lethanh040997@gmail.com>
2021-11-29 11:20:01 +0700

    add chart

--- components/InformationDetail.tsx
+++ components/InformationDetail.tsx
@@ -43,7 +43,7 @@ const InformationDetail = ({ onClose, selectedCode, open }: IDialogProps) => {
       <Box>
 
       
-        {/* <CoviInfo />  */}
+        <CoviInfo /> 
      
      
       </Box>
